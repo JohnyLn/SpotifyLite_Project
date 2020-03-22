244a67d8c11d6324f1ceae35cc2d4518241cd8b2
JohnLn <johny.lin37@gmail.com>
2020-03-22 14:44:39 +0100

    First initiate

---
+++ SpotifyLite/app/src/main/java/fr/esilv/spotifylite/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.esilv.spotifylite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
